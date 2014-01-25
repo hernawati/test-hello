acff352e5e550775740a98bb59930fb5fd9b50ee
herna <hernawatisamosir@gmail.com>
2014-01-24 16:44:14 -0800

    Penambahan proyek HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+	public static void main (String[]args){
+		System.out.println("Hello World Indo");
+		JOptionPane.showMessageDialog(null,"Testing");
+	}
+}
